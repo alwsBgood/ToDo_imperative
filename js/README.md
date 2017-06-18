03d046490fd6ffdff39d544c90b2a263eb1b8329
Dmitriy <dima.chebotaru@gmail.com>
2017-06-18 20:55:17 +0300

    Few functions in app.js

--- js/README.md
+++
@@ -1,3 +0,0 @@
-I'm going to make a banch of ToDo Apps in different coding style or it is better to say in different architecture types. The first one is imperative style.
-
-Inspired by CodeDojo
--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
+function createTodoItem(title) {
+  const checkbox = documnet.createElement('input');
+}
+
+function addTodoItem(event) {
+  event.preventDefault();
+
+  if (!addInput.value) {
+    return alert('Необходимо ввести название задачи')
+  }
+  const listItem = createTodoItem(addInput.value);
+}
+
+
 const todoForm = document.getElementById('todo-form');
-const addinput = document.getElementById('add-input');
+const addInput = document.getElementById('add-input');
 const todoList = document.getElementById('todo-list');
 const todoItems = document.querySelectorAll('.todo-item');
 
-
+todoForm.addEventListener('submit', addTodoItem);
