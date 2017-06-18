de20202fd9b92e4c1bc415177100ba5799e07707
Dmitriy <dima.chebotaru@gmail.com>
2017-06-18 20:41:13 +0300

    Index.html file + styles

---
+++ js/README.md
@@ -0,0 +1,3 @@
+I'm going to make a banch of ToDo Apps in different coding style or it is better to say in different architecture types. The first one is imperative style.
+
+Inspired by CodeDojo
---
+++ js/app.js
@@ -0,0 +1,6 @@
+const todoForm = document.getElementById('todo-form');
+const addinput = document.getElementById('add-input');
+const todoList = document.getElementById('todo-list');
+const todoItems = document.querySelectorAll('.todo-item');
+
+
